08831d5143a082c03d63ebb078e58ee0057c4e58
AbylayEsh <abylaiesh.07@gmail.com>
2025-02-09 21:37:45 +0600

    Add 2nd HW

--- js/home_works.js
+++ js/home_works.js
@@ -25,20 +25,82 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-//HW1 Part 2
+//HW1 Part 2 || HW2 Part 1
 const childBlock = document.querySelector(".child_block");
 const parentBlock = document.querySelector(".parent_block");
 
 let positionX = 0;
+let positionY = 0;
 
 const offsetWidth = parentBlock.clientWidth - childBlock.clientWidth;
+const offsetHeight = parentBlock.clientHeight - childBlock.clientHeight;
+
+const speed = 2;
 
 const moveBlock = () => {
-    if (positionX < 90) {
-            positionX++;
-            childBlock.style.left = `${positionX}%`;
-            requestAnimationFrame(moveBlock)
+    if (positionX < offsetWidth && positionY === 0) {
+        positionX += speed;
+        childBlock.style.left = `${positionX}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionX >= offsetWidth && positionY < offsetHeight) {
+        positionY += speed;
+        childBlock.style.top = `${positionY}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionY >= offsetHeight && positionX > 0) {
+        positionX -= speed;
+        childBlock.style.left = `${positionX}px`;
+        requestAnimationFrame(moveBlock);
+    } else if (positionX === 0 && positionY > 0) {
+        positionY -= speed;
+        childBlock.style.top = `${positionY}px`;
+        requestAnimationFrame(moveBlock);
+    } else {
+        requestAnimationFrame(moveBlock);
     }
-}
+};
 
-moveBlock ();
+moveBlock()
+
+// HW2 Part2
+
+const startBtn = document.querySelector("#start");
+const stopBtn = document.querySelector("#stop");
+const resetBtn = document.querySelector("#reset");
+const counterDisplay = document.querySelector("#seconds");
+
+let counter = 0;
+let intervalId;
+let isRunning = false;
+
+const updateCounterDisplay = () => {
+    counterDisplay.textContent = counter;
+};
+
+updateCounterDisplay();
+
+startBtn.addEventListener("click",() => {
+    if (!isRunning) {
+        intervalId = setInterval (() => {
+            counterDisplay.style.color = "green";
+            counter++;
+            updateCounterDisplay();
+        },1000)
+        isRunning = true;
+    }
+})
+
+stopBtn.addEventListener("click",() => {
+    if (isRunning) {
+        counterDisplay.style.color = "red";
+        clearInterval(intervalId);
+        isRunning = false;
+    }
+})
+
+resetBtn.addEventListener("click", () => {
+    counterDisplay.style.color = "white";
+    clearInterval(intervalId);
+    counter = 0;
+    updateCounterDisplay();
+    isRunning = false;
+});
